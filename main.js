2bfa09076d648e58726fca457fc766ba2b249844
Luannaquinuparocha <128722913+Luannaquinuparocha@users.noreply.github.com>
2023-10-05 11:49:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while () {
+    listaDeTeclas[0].onclick = tocaSomPom;
+}
